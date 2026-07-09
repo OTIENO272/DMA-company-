05b69cd8a31653e211034714d1bec36002691d50
Boateng@37 <otienov272@gmail.com>
2026-07-09 12:16:44 +0300

    Added a jwt

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,11 +1,21 @@
 import User from "../models/userModel.js";
+import jwt from 'jsonwebtoken'
 
+//tokenization
+const signToken= (_id)=>{
+  return  jwt.sign({_id},process.env.SECRET,{"expiresIn":"3d"})
+}
 
 const userSignUp=async(req,res)=>{
    try {
+    
     const {username,email,password,confirmPassword} = req.body
      const user = await User.signup(username,email,password,confirmPassword)
-     res.status(200).json({user})
+
+     //token creation
+     const token = await signToken(user._id)
+
+     res.status(200).json({email,token})
    } catch (error) {
     res.status(400).json({error:error.message})
    }
@@ -15,10 +25,15 @@ const userLogin=async(req,res)=>{
     try {
         const {email,password} =req.body
         const user = await User.login(email,password)
-        res.status(200).json({user})
+         //token creation
+      const token = await signToken(user._id)
+
+      res.status(200).json({email,token})
     } catch (error) {
      res.status(400).json({error:error.message})
     }
 }
+
+
 
 export {userSignUp,userLogin}

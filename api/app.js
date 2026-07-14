b3f91e805c5c26848e1b7f0f25e79a9b34392ac0
Boateng@37 <otienov272@gmail.com>
2026-07-14 18:18:49 +0300

    Fix CORS configuration for Vercel deployment

--- api/app.js
+++ api/app.js
@@ -9,17 +9,26 @@ const app = express()
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://dma-labs-irxq-three.vercel.app/"
+  "https://dma-labs-irxq-three.vercel.app",
 ];
 
 app.use(express.json())
 
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
-app.use(cors(
-   { origin: allowedOrigins,
-     credentials:true}
-))
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests from Postman or server-to-server
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    callback(new Error("Not allowed by CORS"));
+  },
+  credentials: true,
+}));
 
 
 app.use(userRouter)
